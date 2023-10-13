dd7510a8552a2e217a7ba95b4ff1191dce0e2ae4
FabiRomano <99614961+FabiRomano@users.noreply.github.com>
2023-10-13 17:58:13 -0300

    css-modificado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
+
 export class HeaderComponent {
 
+  
+  stars: { x: number, y: number }[] = [];
+
+  @HostListener('document:mousemove', ['$event'])
+  addStar(event: MouseEvent) {
+    const inicio = document.getElementById('inicioo');
+    if (inicio) {
+      this.stars.push({ x: event.clientX, y: event.clientY });
+
+      setTimeout(() => {
+        this.stars = this.stars.filter(star => star !== this.stars[0]);
+      }, 800);
+    }
+  }
+
+  
 }
